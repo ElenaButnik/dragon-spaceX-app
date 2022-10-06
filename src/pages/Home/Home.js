8a822fd06165d7a2da021876f1d8356121373f40
ElenaButnik <butnik.elena@gmail.com>
2022-10-07 02:16:21 +0300

    fixed home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,19 @@
-import { useAuth } from "../../hooks/use-auth";
+import { useSelector } from "react-redux";
+import { getIsLoggedIn } from "../../redux/User/selectors";
 import { Register } from "../RegisterPage/RegisterPage";
 import s from "./Home.module.css";
 
 export function Home() {
-  const { isAuth, email } = useAuth();
+  const isLoggedIn = useSelector(getIsLoggedIn);
 
-  return  (
+  return (
     <div className={s.Section}>
       <h1 className="animate__animated animate__slideInUp"> DRAGON</h1>
       <h2 className="animate__animated animate__slideInUp">
         {" "}
         SENDING HUMANS AND CARGO INTO SPACE{" "}
       </h2>
-      {!isAuth ? <Register /> : null}
+      {!isLoggedIn ? <Register /> : null}
     </div>
-  ) 
+  );
 }
